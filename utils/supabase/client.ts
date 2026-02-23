317ab777e6ded9ca6a736b679ec34117f15d7f75
jjohnson-lumenclarity <jjohnson@lumenclarity.com>
2026-02-23 15:53:39 -0500

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,4 +1,7 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { createClient } from '@supabase/supabase-js'
 
-export const createClient = () => createClientComponentClient();
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
+export const createClient = () => 
+  createClient(supabaseUrl, supabaseKey)
